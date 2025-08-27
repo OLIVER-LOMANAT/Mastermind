e384cc2506962d691c3e5d127230698876434934
Oliver Lomanat <lomanatoliver@gmail.com>
2025-08-27 10:18:13 +0300

    importing inbuilt methods

---
+++ lib/models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from datetime import datetime
+
