664fbb9c7f9233b3c5a7d47cf6d25c3244ddfa0b
Oliver Lomanat <lomanatoliver@gmail.com>
2025-08-27 10:50:15 +0300

    defining the scret number and the check guess function

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,3 +1,18 @@
 import random
 from models import session, Player, Game, Guess
 
+def generate_secret_number():
+    digits = list('123456789')
+    random.shuffle(digits)
+    return ''.join(digits[:4])
+
+def check_guess(secret, guess):
+    bulls =0
+    cows = 0
+    for i in range(4):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+    return bulls, cows
+
